eb5294ebf5dfff5870df221b1736c13f0356af6a
ajjnix <ajjnix@gmail.com>
2015-05-24 14:52:10 +0700

    Initial Commit

---
+++ DemoUnitTesting/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoUnitTesting
+//
+//  Created by ajjnix on 24.05.15.
+//  Copyright (c) 2015 ajjnix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoUnitTesting/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoUnitTesting
+//
+//  Created by ajjnix on 24.05.15.
+//  Copyright (c) 2015 ajjnix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
